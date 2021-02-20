ec0f9ac1aedf417f0bf9a48247a62b52c110e774
Nicolas DEUTSCHMANN <ndeutschmann@hotmail.com>
2021-02-20 17:59:47 +0100

    added setup.py

---
+++ src/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("../README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="slides", # Replace with your own username
+    version="0.1",
+    author="Nicolas Deutschmann",
+    description="Javascript slide web-app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ndeutschmann-fun/slides_webapp",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
